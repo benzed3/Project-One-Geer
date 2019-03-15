8d9005eaefe06cfb32347478d2f27bf0a28a49b6
benzed3 <benzed3@gmail.com>
2019-03-15 19:48:29 -0400

    almost finished

--- GEER.js
+++ GEER.js
@@ -1,25 +1,29 @@
 function displayInfo() {
 
-    $("button").on("click", function (event) {
+    $(".list-group-item").empty();
 
-        event.preventDefault();
+    var queryURL = "https://cors-anywhere.herokuapp.com/http://api.eventful.com/json/events/search?app_key=ZM6QM7Wzsb7rrLZD&keywords=books&location=Charlotte&date=This+Week&within=15&sort_order=popularity&page_size=6&page_number=1";
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    })
+        .then(function (response) {
 
-        var queryURL = "https://cors-anywhere.herokuapp.com/http://api.eventful.com/json/events/search?app_key=ZM6QM7Wzsb7rrLZD&keywords=books&location=Charlotte&date=This+Week&within=15&sort_order=popularity&page_size=6&page_number=1";
-        $.ajax({
-            url: queryURL,
-            method: "GET"
-        })
-            .then(function (response) {
+            var jsonResponse = JSON.parse(response);
 
-                console.log(JSON.parse(response));
+            console.log(jsonResponse.events);
 
-                var events;
+            var eventImage = $("<img>");
 
+            eventImage.attr("src", jsonResponse.events.event[0].image.small);
+            eventImage.attr("src", jsonResponse.events.event[0].performers.performer);
+            eventImage.attr("src", jsonResponse.events.event[0].description)
 
+            $(".card-image-top").append(eventImage);
+            $(".list-group-item").text(JSON.stringify(eventImage));
+            $(".card-text").text(JSON.stringify(eventImage));
 
-            });
-    });
-
+        });
 };
 
 displayInfo();
