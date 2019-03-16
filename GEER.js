850be1751886bb1cab79b7c2c6ed0718a81d506c
glennbackus <46578905+glennbackus@users.noreply.github.com>
2019-03-16 12:59:48 -0400

    FINAL

--- GEER.js
+++ GEER.js
@@ -1,5 +1,5 @@
 function displayInfo() {
-    var queryURL = "https://cors-anywhere.herokuapp.com/http://api.eventful.com/json/events/search?app_key=ZM6QM7Wzsb7rrLZD&keywords=books&location=Charlotte&date=This+Week&within=15&sort_order=popularity&page_size=6&page_number=1&mature=safe&category=Festival";
+    var queryURL = "https://immense-savannah-32940.herokuapp.com/http://api.eventful.com/json/events/search?app_key=ZM6QM7Wzsb7rrLZD&keywords=books&location=Charlotte&date=This+Week&within=15&sort_order=popularity&page_size=6&page_number=1&mature=safe&category=Festival";
     $.ajax({
         url: queryURL,
         method: "GET"
@@ -107,7 +107,7 @@ window.addEventListener("load", () => {
         navigator.geolocation.getCurrentPosition(position => {
             long = position.coords.longitude;
             lat = position.coords.latitude;
-            const proxy = "http://cors-anywhere.herokuapp.com/";
+            const proxy = "https://immense-savannah-32940.herokuapp.com/";
             const api = `${proxy}https://api.darksky.net/forecast/b49da12cb5042bef75af1a465390acf6/37.8267,-122.4233`;
             fetch(api)
                 .then(response => {
@@ -118,7 +118,6 @@ window.addEventListener("load", () => {
                     const { temperature, summary } = data.currently;
                     temperatureDegree.textContent = temperature;
                     temperatureDescription.textContent = summary;
-                    locationTimezone.textContent = data.timeZone;
                 })
         });
     } else {
